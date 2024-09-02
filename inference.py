9c0394dd39ef56fdf320a4a4f37364c96493c8bf
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2024-09-02 22:32:37 +0530

    Updates in Inference file

--- inference.py
+++ inference.py
@@ -10,7 +10,7 @@ from LSTM_on_Collected_data import LSTM_Phasic, Prepare_Data
 # # Collect Data
 # data_file_path = main()
 
-data_file_path = "./Collected_Data/Horror_Srushti.csv"
+data_file_path = "./Collected_Data/Horror_Tanishqua.csv"
 
 # %%
 # Prepare Data
@@ -32,5 +32,5 @@ print(predictions)
 # histogram
 plt.figure(figsize = (10,2))
 plt.bar(x=np.arange(len(predictions)),height=predictions)
-plt.title("Arousal Histogram")
+plt.title("Arousal Activity Plot")
 plt.show()
