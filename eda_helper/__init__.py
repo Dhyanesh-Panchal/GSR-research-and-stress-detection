5a22d24424b33149af8e32256ffcba8a15060c87
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2024-12-31 11:17:45 +0530

    standard function

--- eda_helper/__init__.py
+++ eda_helper/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from tqdm.notebook import tqdm as bar
+from tqdm.notebook import tqdm as tqdm
 
 
 class Processing:
@@ -9,7 +9,7 @@ class Processing:
 
     @staticmethod
     def maximum_absolute_scaling(data: pd.DataFrame, colum_names):
-        for colum_name in bar(colum_names, desc="Scaling column values"):
+        for colum_name in tqdm(colum_names, desc="Scaling column values"):
             # shift the data to zero mean
             data[colum_name] = data[colum_name] - np.mean(data[colum_name])
             # divide by max +ve value present to get in range [-1,1]
@@ -25,7 +25,7 @@ class Processing:
     def generate_windows_df(df: pd.DataFrame, window_size: int, window_gap: int, column_name: str):
         def rolling_window(df: pd.DataFrame, window_size: int, window_gap: int):
             windows = []
-            for i in bar(range(0, len(df)-window_size+1, window_gap), desc="generating window"):
+            for i in tqdm(range(0, len(df)-window_size+1, window_gap), desc="generating window"):
                 single_window = df.iloc[i:i+window_size]
                 # print(single_window)
                 windows.append(single_window)
@@ -35,7 +35,7 @@ class Processing:
             windowed_data = rolling_window(
                 df, window_size, window_gap)
             array = []
-            for frame in bar(windowed_data, desc="preparing dataframe"):
+            for frame in tqdm(windowed_data, desc="preparing dataframe"):
                 array.append(list(frame[column_name]))
 
             window_df = pd.DataFrame(np.array(array))
@@ -63,3 +63,40 @@ class Processing:
         X,y = _pre_process(filtered_data)
         y = pd.get_dummies(y)
         return X,y
+
+    @staticmethod
+    def prepare_windows_with_labels(
+        data,
+        x_column="EDA_Phasic",
+        y_columns=["arousal", "valence"],
+        y_aggregator = "mean",
+        window_size=100,
+        window_gap=1,
+    ):
+        '''
+        
+        '''
+        # prepare the x column
+        x_df = Processing.generate_windows_df(data, window_size, window_gap, x_column)
+        
+        # prepare the y columns
+        y_dfs = []
+        for y_column in tqdm(y_columns, desc="Preparing Y Columns:"):
+            y_df = Processing.generate_windows_df(data, window_size, window_gap, y_column)
+            # Handle the aggregation based on the type of y_aggregator
+            if isinstance(y_aggregator, str): # for standard functions
+                y_df = y_df.agg(y_aggregator, axis=1)
+            else:
+                # For custom functions, use apply to avoid the FutureWarning
+                y_df = y_df.apply(y_aggregator, axis=1)
+            assert type(y_df) == pd.Series
+            y_dfs.append(y_df)
+        y_df = pd.concat(y_dfs, axis=1)
+        y_df.columns = y_columns
+        
+        assert x_df.shape[0] == y_df.shape[0]
+        # merge the x and y columns
+        final_df = pd.concat([x_df,y_df], axis=1)
+
+    
+        return final_df
